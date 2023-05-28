a9dc436d4349eb12a62c9c920944e49d7e658fc1
arun-particlephy <arun.particlephy@tamu.edu>
2023-05-28 00:08:28 -0500

    Strat and end ranges of times

--- dataanalysis.py
+++ dataanalysis.py
@@ -4,6 +4,45 @@ import seaborn as sns
 import datetime as dt
 
 
+def find_ranges(df, column, eps, value_column):
+    """
+
+    :param df:
+    :param column:
+    :param eps:
+    :param value_column:
+    :return:
+    """
+    # Initialize variables
+    ranges = []
+    start_range = None
+    end_range = None
+
+    # Iterate over each value in the column
+    for i, value in enumerate(df[column]):
+        if value > eps:
+            if start_range is None:
+                # Start a new range
+                start_range = df[value_column].iloc[i]
+                end_range = df[value_column].iloc[i]
+            else:
+                # Expand the current range
+                end_range = df[value_column].iloc[i]
+        elif start_range is not None:
+            # End the current range
+            ranges.append((start_range, end_range))
+            start_range = None
+            end_range = None
+
+    # Check if there is an open range at the end
+    if start_range is not None:
+        ranges.append((start_range, end_range))
+
+    # Create a new DataFrame to store the ranges
+    result_df = pd.DataFrame(ranges, columns=['Start Range', 'End Range'])
+    return result_df
+
+
 # function to convert HHMM to datetime.time
 def format_time(x):
     hhmm = dt.time(x)
@@ -24,7 +63,16 @@ test['DATETIME'] = test.apply(lambda r: dt.datetime.combine(r['DATETIME'], r['ti
 test = test.sort_values(by=['DATETIME'], ignore_index=True)
 test = test.drop(columns=['time'])
 
-## Store only values greater than 5
-test_storms = test[test['Kp'] > 5]
+# Store only values greater than 5
+
+threshold = 5
+stormTimeRanges = find_ranges(test, 'Kp', threshold, 'DATETIME')
+
+# time delta between two time ranges
+stormTimeRanges['MGMDT'] = stormTimeRanges['End Range'] - stormTimeRanges['Start Range']
+
+# convert the time delta to integer hours and add 3 hours to include one of the end ranges
+stormTimeRanges['MGMDT'] = stormTimeRanges['MGMDT'].astype('timedelta64[s]').astype('int64') / 3600 + 3
+
 
 k = 1
